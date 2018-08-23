4e8cdc715e3354e5a740790a413957b3f47cf1be
Yanis HEDJEM <yanishproduction@gmail.com>
2018-08-23 11:12:05 +0200

    feat(example): add example

---
+++ example/server/main.go
@@ -0,0 +1,43 @@
+/*
+
+In this file, we are going to create a simple Dumby server.
+It will print the client's message and reply back.
+
+
+Go ahead, even a Dumb can do that!
+
+*/
+
+package main
+
+import (
+	"fmt"
+
+	"github.com/yanishoss/dumby/protocol"
+	"github.com/yanishoss/dumby/server"
+)
+
+const (
+	// The address the server will listen to
+	host = "localhost:8080"
+
+	actionHello = iota + 2
+)
+
+func main() {
+	s := server.New(&server.Config{
+		MaxConnections: 5000,
+	})
+
+	s.AddHandlers(actionHello, func(trame *protocol.Trame, s chan<- *protocol.Trame) {
+		// Print the message sent by the client
+		fmt.Println("Someone says: ", string(trame.Payload))
+
+		// Respond back with a message
+		s <- protocol.New(trame.Session, actionHello, []byte("Hello my friend! Is Dumby the best protocol on this world ?"))
+	})
+
+	s.Listen(host)
+
+	// So it's not difficult, right ?
+}
